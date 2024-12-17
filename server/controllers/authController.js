825383b55672f18958c4951df5e74d3aab85e779
elyse502 <elyseniyibizi502@gmail.com>
2024-12-17 18:58:40 +0200

    Create User Email verification Controller Function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -135,3 +135,40 @@ export const sendVerifyOtp = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+export const verifyEmail = async (req, res) => {
+  const { userId, otp } = req.body;
+
+  if (!userId || !otp) {
+    return res.json({
+      success: false,
+      message: "Missing Details",
+    });
+  }
+
+  try {
+    const user = await userModel.findById(userId);
+
+    if (!user) {
+      return res.json({ success: false, message: "User Not Found" });
+    }
+
+    if (user.verifyOtp === "" || user.verifyOtp !== otp) {
+      return res.json({ success: false, message: "Invalid OTP" });
+    }
+
+    if (user.verifyOtpExpireAt < Date.now()) {
+      return res.json({ success: false, message: "OTP Expired" });
+    }
+
+    user.isAccountVerified = true;
+    user.verifyOtp = "";
+    user.verifyOtpExpireAt = 0;
+
+    await user.save();
+
+    return res.json({ success: true, message: "Email Verified Successfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
