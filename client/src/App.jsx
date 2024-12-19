5f8ccfe965f65f3308dd9c5da995f5493e35a546
elyse502 <elyseniyibizi502@gmail.com>
2024-12-19 09:00:25 +0200

    Structure Project(Add Pages)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
   return (
-    <div className='text-4xl text-blue-600'>
-      Hello, World!
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
