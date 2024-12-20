9f5dfabddeae691c8f56d105881dd1529b0081d2
elyse502 <elyseniyibizi502@gmail.com>
2024-12-20 08:57:21 +0200

    Set the domain option in the CORS configuration to null

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true, domain: null }));
 
 // API Endpoints
 app.get("/", (req, res) => res.send("API Working"));
